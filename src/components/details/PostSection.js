1e1b22b825b9b072984c845cd4a2f7b027f2cc35
Max Pinheiro <maxpinheiro181@gmail.com>
2020-12-12 13:41:54 -0500

    posts data

--- src/components/details/PostSection.js
+++ src/components/details/PostSection.js
@@ -8,29 +8,70 @@ import postService from '../../services/PostService';
 class Post extends React.Component {
     state = {
         searchQuery: "",
+        error: "",
         post: {
-
+            id: "",
+            type: "",
+            artist: "",
+            artistId: "",
+            title: "",
+            text: ""
         }
     };
 
     componentDidMount() {
         const detailId = this.props.detailId;
         postService.findPostById(detailId)
-            .then(post => this.setState(prevState => ({
-                ...prevState,
-                post
-            })));
+            .then(post => {
+                if (!post.error) {
+                    this.setState(prevState => ({
+                        ...prevState,
+                        post
+                    }))
+                } else {
+                    this.setState(prevState => ({
+                        ...prevState,
+                        error: "There is no detail with this ID.",
+                        post: null
+                    }))
+                }
+
+            });
     }
 
     render() {
         return (
             <div className="container-fluid">
                 <span>
-                    <p className="h3 d-inline mr-2">Album Details</p>
+                    <p className="h3 d-inline mr-2">Post Details</p>
                     <Link to={`/search?${queryString.stringify({criteria: this.props.searchQuery})}`} className="mx-2">Back to results</Link>
                     <Link to="/search" className="mx-2">Search for something else</Link>
                 </span>
-
+                {
+                    this.state.error !== "" &&
+                    <p className="p-2 rounded border-secondary text-danger">{this.state.error}</p>
+                }
+                <div className="border border-2 border-secondary container-fluid mt-2">
+                    <div className="m-2">
+                        <div className="row">
+                            <p className="h2 my-auto">
+                                {this.state.post.title}
+                            </p>
+                            <p className="h4 mx-4 my-auto">
+                                By
+                                <Link to={`/profile/${this.state.post.artistId}`} className="ml-2">{this.state.post.artist}</Link>
+                            </p>
+                            <p className="text-body my-auto">
+                                Type: {this.state.post.type}
+                            </p>
+                        </div>
+                        <div className="row">
+                            <p className="text-body my-auto">
+                                {this.state.post.text}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
