cbc4e2dbf44dc294bd4273ce3afd6d2d61923e24
Max Pinheiro <maxpinheiro181@gmail.com>
2020-12-12 14:53:28 -0500

    post & review detail pages, logged-in profile redirection

--- src/components/details/ReviewSection.js
+++ src/components/details/ReviewSection.js
@@ -3,28 +3,72 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import queryString from "querystring";
 
+import reviewService from '../../services/ReviewService';
+
 class Review extends React.Component {
     state = {
         searchQuery: "",
+        error: "",
         review: {
-
+            id: "",
+            creator: "",
+            creatorId: "",
+            songId: "",
+            title: "",
+            text: ""
         }
     };
 
     componentDidMount() {
         const detailId = this.props.detailId;
-        //
+        reviewService.findReviewById(detailId)
+            .then(review => {
+                if (!review.error) {
+                    this.setState(prevState => ({
+                        ...prevState,
+                        review
+                    }))
+                } else {
+                    this.setState(prevState => ({
+                        ...prevState,
+                        error: "There is no review with this ID.",
+                        review: null
+                    }))
+                }
+
+            });
     }
 
     render() {
         return (
             <div className="container-fluid">
                 <span>
-                    <p className="h3 d-inline mr-2">Album Details</p>
+                    <p className="h3 d-inline mr-2">Review Details</p>
                     <Link to={`/search?${queryString.stringify({criteria: this.props.searchQuery})}`} className="mx-2">Back to results</Link>
                     <Link to="/search" className="mx-2">Search for something else</Link>
                 </span>
-
+                {
+                    this.state.error !== "" &&
+                    <p className="p-2 rounded border-secondary text-danger">{this.state.error}</p>
+                }
+                <div className="border border-2 border-secondary container-fluid mt-2">
+                    <div className="m-2">
+                        <div className="row">
+                            <p className="h2 my-auto">
+                                {this.state.review.title}
+                            </p>
+                            <p className="h4 mx-4 my-auto">
+                                By
+                                <Link to={`/profile/${this.state.review.creatorId}`} className="ml-2">{this.state.review.creator}</Link>
+                            </p>
+                        </div>
+                        <div className="row">
+                            <p className="text-body my-auto">
+                                {this.state.review.text}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
